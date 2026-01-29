488ac7e20263e7912f2add070b8621c1555cc459
Patricio Dutra <patricio.dutra@afya.com.br>
2026-01-29 05:30:11 -0300

    Fix Spotless config and make project JitPack compatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,20 +13,16 @@
 // limitations under the License.
 
 group = "com.github.afya-educon"
-version = "1.5.2-v5"
+version = "1.5.2-v6"
 
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.nexus.plugin)
-  //alias(libs.plugins.spotless)
   alias(libs.plugins.dokka)
   alias(libs.plugins.kotlin.binary.compatibility)
 }
-
-//apply(from ="${rootDir}/scripts/publish-root.gradle")
 
 apiValidation {
   ignoredPackages.add("com/skydoves/balloon/databinding")
@@ -34,29 +30,5 @@ apiValidation {
 }
 
 subprojects {
-  //apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
-
-  configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-    kotlin {
-      target("**/*.kt")
-      targetExclude("$buildDir/**/*.kt")
-      ktlint().setUseExperimental(true).editorConfigOverride(
-        mapOf(
-          "indent_size" to "2",
-          "continuation_indent_size" to "2"
-        )
-      )
-      licenseHeaderFile(rootProject.file("spotless/spotless.license.kt"))
-      trimTrailingWhitespace()
-      endWithNewline()
-    }
-    format("xml") {
-      target("**/*.xml")
-      targetExclude("**/build/**/*.xml")
-      // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
-      licenseHeaderFile(rootProject.file("spotless/spotless.license.xml"), "(<[^!?])")
-      trimTrailingWhitespace()
-      endWithNewline()
-    }
-  }
+  apply(plugin = "maven-publish")
 }
